3c646b1dc58f59585f3f039ea0435a04e4dd5836
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-16 09:16:10 -0400

    adjusted phrases

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -28,7 +28,7 @@ const Thermostat = () => {
         
         <div style={boxStyle}>
             <h1>{temp}°</h1>
-            <h3>{temp < 32 ? 'Freezing' : temp < 65 ? 'Cold' : temp < 70 ? 'Cool' : temp < 73 ? 'Just Right' : temp < 78 ? 'Warm' : 'Hot'}</h3>
+            <h3>{temp < 32 ? 'Freezing' : temp < 65 ? 'Cold' : temp < 70 ? 'Cool' : temp < 74 ? 'Just Right' : temp < 78 ? 'Warm' : 'Hot'}</h3>
             <div style={buttonStyle}>
                 <button onClick={()=>changeTemp(temp+1)}>+</button>
                 <button onClick={()=>changeTemp(temp-1)}>-</button>
